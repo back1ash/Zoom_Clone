5ec7152437c5bb98a2a27bf633b66d269eff78db
Haejun Lee <teamforhj@gmail.com>
2022-04-25 16:04:18 +0900

    feat: 채팅 기능 구현

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -7,6 +9,27 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function closefn() {
+  return console.log('Disconnected from the Browser');
+}
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  console.log('Connect to Browser');
+  socket.on('close', closefn);
+  socket.on('message', (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
+  socket.send('hello!');
+});
+
+server.listen(3000, handleListen);
